edd50528bb00a2d9323e599124e8426e54623c2b
Dominic Batstone <dobatsto@microsoft.com>
2021-12-09 14:59:19 +0000

    Add project files.

---
+++ Forms Region Sample/SampleFormRegion.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Forms_Region_Sample
+{
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class SampleFormRegion : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public SampleFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Form Region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "SampleFormRegion";
+            manifest.FormRegionType = Microsoft.Office.Tools.Outlook.FormRegionType.Adjoining;
+
+        }
+
+        #endregion
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelInstructions
+            // 
+            this.LabelInstructions.AutoSize = true;
+            this.LabelInstructions.BackColor = System.Drawing.SystemColors.Control;
+            this.LabelInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelInstructions.ForeColor = System.Drawing.Color.Red;
+            this.LabelInstructions.Location = new System.Drawing.Point(15, 4);
+            this.LabelInstructions.Name = "LabelInstructions";
+            this.LabelInstructions.Size = new System.Drawing.Size(107, 20);
+            this.LabelInstructions.TabIndex = 0;
+            this.LabelInstructions.Text = "Text To Send:";
+            // 
+            // SampleFormRegion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LabelInstructions);
+            this.Name = "SampleFormRegion";
+            this.Size = new System.Drawing.Size(817, 36);
+            this.FormRegionShowing += new System.EventHandler(this.FormRegion1_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.FormRegion1_FormRegionClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelInstructions;
+
+        public partial class FormRegion1Factory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public FormRegion1Factory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                SampleFormRegion.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.FormRegion1Factory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                SampleFormRegion form = new SampleFormRegion(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+    partial class WindowFormRegionCollection
+    {
+        internal SampleFormRegion FormRegion1
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(SampleFormRegion))
+                        return (SampleFormRegion)item;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Forms Region Sample/SampleFormRegion.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace Forms_Region_Sample
+{
+    partial class SampleFormRegion
+    {
+        #region Form Region Factory 
+
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Appointment)]
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Note)]
+        [Microsoft.Office.Tools.Outlook.FormRegionName("Forms Region Sample.FormRegion1")]
+        public partial class FormRegion1Factory
+        {
+            // Occurs before the form region is initialized.
+            // To prevent the form region from appearing, set e.Cancel to true.
+            // Use e.OutlookItem to get a reference to the current Outlook item.
+            private void FormRegion1Factory_FormRegionInitializing(object sender, Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs e)
+            {
+            }
+        }
+        #endregion
+
+        // Occurs before the form region is displayed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void FormRegion1_FormRegionShowing(object sender, System.EventArgs e)
+        {
+            try
+            {
+                ItemProperty property = ((dynamic)OutlookItem).ItemProperties["THIS WILL CAUSE AN EXEPTION!!!"];
+                //if var is used instead of ItemProperty it has different behaviour
+            }
+            catch (System.Exception)
+            {
+                //ignore - just to slow down the processing of this method
+            }
+
+            ((ItemEvents_10_Event)OutlookItem).Send += MarkingFormRegion_Send;
+        }
+
+        // Occurs when the form region is closed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void FormRegion1_FormRegionClosed(object sender, System.EventArgs e)
+        {
+            ((ItemEvents_10_Event)OutlookItem).Send -= MarkingFormRegion_Send;
+        }
+
+        private void MarkingFormRegion_Send(ref bool cancel)
+        {
+            MessageBox.Show("Sending");
+        }
+    }
+}
